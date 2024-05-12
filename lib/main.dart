13a4138dd83981ba625c934e4f9d33254f2f19a4
Claylton Santos <claylton.santos@dilettasolutions.com>
2024-05-12 13:26:06 -0300

    Feat: Add input value

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:ethanol_or_gasoline/widgets/logo_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,21 +17,55 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
-      home: const HomePage(),
+      home: HomePage(),
     );
   }
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  final MaskTextInputFormatter _maskTextInputFormatter = MaskTextInputFormatter(
+    mask: '#,##',
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
       body: ListView(
-        children: const <Widget>[
-          LogoWidget(),
+        children: <Widget>[
+          const LogoWidget(),
+          Row(
+            children: [
+              Container(
+                width: 100,
+                alignment: Alignment.centerRight,
+                child: const Text(
+                  "Gasolina",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 35,
+                    fontFamily: "Big Shoulders Display",
+                  ),
+                ),
+              ),
+              const SizedBox(width: 20),
+              Expanded(
+                child: TextFormField(
+                  initialValue: "0,00",
+                  inputFormatters: [_maskTextInputFormatter],
+                  keyboardType: TextInputType.number,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 45,
+                    fontFamily: "Big Shoulders Display",
+                  ),
+                  decoration: const InputDecoration(border: InputBorder.none),
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
